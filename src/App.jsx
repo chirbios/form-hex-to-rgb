d247dc026ffe7a57e01d9f0edd55b870c7c6fad6
Alex <kazzon95@bk.ru>
2023-03-08 11:44:40 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,73 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+import './App.css';
 
-import './App.css'
+function hexToRgb(hex) {
+  const hexfull = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const hexany = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  console.log(result);
+  return result
+    ? 'rgb(' +
+        parseInt(result[1], 16) +
+        ', ' +
+        parseInt(result[2], 16) +
+        ', ' +
+        parseInt(result[3], 16) +
+        ')'
+    : 'Ошибка!';
+}
+
+class BackgroundColor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgcolor: '',
+      rgbcolor: '',
+      errmsg: '',
+    };
+  }
+
+  changeColor(e) {
+    e.preventDefault();
+    if (e.target.value.length >= 7) {
+      this.setState({
+        bgcolor: e.target.value,
+        rgbcolor: hexToRgb(e.target.value),
+      });
+    } else {
+      this.setState({
+        bgcolor: '',
+        rgbcolor: '',
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="render" style={{ backgroundColor: this.state.bgcolor }}>
+        {/* <div class="json">
+          <pre>{JSON.stringify(this.state, null, ' ')}</pre>
+        </div> */}
+        <div className="hex">
+          <input
+            type="text"
+            onChange={(e) => {
+              this.changeColor(e);
+            }}
+          />
+        </div>
+        <div className="rgb">
+          <div className="msg">{this.state.rgbcolor}</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default function App() {
-  const [hex, setHex] = useState('');
-
-  function convertToRGB(){
-  if(hex.length != 6){
-      throw "Ошибка!"
-  }
-  let  aRgbHex = hex.match(/.{1,2}/g);
-  let aRgb = [
-      parseInt(aRgbHex[0], 16),
-      parseInt(aRgbHex[1], 16),
-      parseInt(aRgbHex[2], 16)
-  ];
-  console.log(aRgb)
-  return aRgb;
+  return <BackgroundColor />;
 }
-
-  return (
-    <form onSubmit={e => setHex(e.target.value)}
-      style={{background: `#${hex}`}}>
-      <input 
-        placeholder='Введите цвет'
-        maxLength={6}
-        id='name' name='name' 
-        value={hex}
-        onChange={e => setHex(e.target.value)}/>
-      <label htmlFor='name'>{convertToRGB}</label>
-    </form>
-  )
-}
-
-// setRgb(function hex2rgb(c) {
-//   let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(c);
-//   return result ? {
-//       r: parseInt(result[1], 16),
-//       g: parseInt(result[2], 16),
-//       b: parseInt(result[3], 16)
-//   } : 'Ошибка';
-// })
